0874910e88b08db9b8910a84a20e96a8140fac1c
sairandhree <sairandhree.sule@gmail.com>
2026-09-13 14:57:28 +0530

    adding endpoint for searching by item name

--- src/main/java/com/saka/qms/repository/NameOfItemRepository.java
+++ src/main/java/com/saka/qms/repository/NameOfItemRepository.java
@@ -3,5 +3,8 @@ package com.saka.qms.repository;
 import com.saka.qms.model.NameOfItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NameOfItemRepository extends JpaRepository<NameOfItem, Integer> {
+    List<NameOfItem> findByNameOfItemContainingIgnoreCase(String name);
 }
--- src/main/java/com/saka/qms/web/NameOfItemController.java
+++ src/main/java/com/saka/qms/web/NameOfItemController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -33,6 +34,15 @@ public class NameOfItemController {
     @GetMapping
     public List<NameOfItem> findAll(Authentication authentication) {
         return accessControl.visibleItems(authentication, repository.findAll());
+    }
+
+    @GetMapping("/search")
+    public List<NameOfItem> search(
+            @RequestParam String name,
+            Authentication authentication) {
+        return accessControl.visibleItems(
+                authentication,
+                repository.findByNameOfItemContainingIgnoreCase(name));
     }
 
     @GetMapping("/{id}")
